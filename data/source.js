7bb983fae75fae121be748f055974991283ef07a
Krasavchik <ml.videaux@gmail.com>
2016-12-11 19:29:54 +0100

    add source.js

---
+++ data/source.js
@@ -0,0 +1,122 @@
+var words = [
+  {
+    "ideogram": "饿",
+    "pinyin": "è",
+    "traduction": "avoir faim",
+    "leçon": "0"
+  },
+  {
+    "ideogram": "累",
+    "pinyin": "lèi",
+    "traduction": "être fatigué",
+    "leçon": "0"
+  },
+  {
+    "ideogram": "饭",
+    "pinyin": "fàn",
+    "traduction": "riz",
+    "leçon": "0"
+  },
+  {
+    "ideogram": "汤",
+    "pinyin": "tāng",
+    "traduction": "soupe",
+    "leçon": "0"
+  },
+  {
+    "ideogram": "菜",
+    "pinyin": "cài",
+    "traduction": "plat de résistance",
+    "leçon": "0"
+  },
+  {
+    "ideogram": "面包",
+    "pinyin": "miànbāo",
+    "traduction": "pain",
+    "leçon": "0"
+  },
+  {
+    "ideogram": "书",
+    "pinyin": "shū",
+    "traduction": "livre",
+    "leçon": "0"
+  },
+  {
+    "ideogram": "买",
+    "pinyin": "mǎi",
+    "traduction": "acheter",
+    "leçon": "0"
+  },
+  {
+    "ideogram": "笔",
+    "pinyin": "bǐ",
+    "traduction": "crayon",
+    "leçon": "0"
+  },
+  {
+    "ideogram": "报",
+    "pinyin": "bào",
+    "traduction": "journal",
+    "leçon": "0"
+  },
+  {
+    "ideogram": "裤子",
+    "pinyin": "kùzi",
+    "traduction": "pantalon",
+    "leçon": "0"
+  },
+  {
+    "ideogram": "什么时候",
+    "pinyin": "shénme shíhou",
+    "traduction": "quand / quelle heure",
+    "leçon": "18"
+  },
+  {
+    "ideogram": "回来",
+    "pinyin": "huílai",
+    "traduction": "rentrer",
+    "leçon": "18"
+  },
+  {
+    "ideogram": "告诉",
+    "pinyin": "gàosu",
+    "traduction": "avertir",
+    "leçon": "18"
+  },
+  {
+    "ideogram": "病",
+    "pinyin": "bìng",
+    "traduction": "malade",
+    "leçon": "18"
+  },
+  {
+    "ideogram": "这几天",
+    "pinyin": "zhè-jǐ tiān",
+    "traduction": "ces jours ci",
+    "leçon": "18"
+  },
+  {
+    "ideogram": "舒服",
+    "pinyin": "shūfu",
+    "traduction": "à l'aise / en forme",
+    "leçon": "18"
+  },
+  {
+    "ideogram": "姓",
+    "pinyin": "xìng",
+    "traduction": "nom de famille",
+    "leçon": "18"
+  },
+  {
+    "ideogram": "叫",
+    "pinyin": "jiào",
+    "traduction": "prénom",
+    "leçon": "18"
+  },
+  {
+    "ideogram": "一定",
+    "pinyin": "yídìng",
+    "traduction": "sans faute",
+    "leçon": "18"
+  }
+];
